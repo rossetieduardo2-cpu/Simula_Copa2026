94970276ddeaca285ed9b7efb9933cc4f6da5a9a
rossetieduardo2-cpu <rossetieduardo2@gmail.com>
2026-07-06 00:09:26 -0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -144,6 +144,8 @@
     { timeA: "Marrocos", gA: 3, timeB: "Canadá", gB:0},
     { timeA: "França", gA:1, timeB:"Paraguai", gB:0},
     { timeA: "Brasil", gA:1, timeB:"Noruega", gB:2},
+    { timeA: "México", gA:2, timeB:"Inglaterra", gB:3},
+    
     
   ];
 
